65c4f989948cd9de2b71a7ef0cdc7a7ae1a03635
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2022-07-05 11:36:38 +0530

    Created question 1 code
    prefix operator not working

---
+++ Tutorial_2/question1.scala
@@ -0,0 +1,24 @@
+object question1 extends App{
+    import scala.language.postfixOps;
+
+    var k ,i ,j = 2;    //variable declaration and initialization
+    var m ,n = 5;
+    var f = 12.0f;
+    var g = 4.0f;
+    var x = 'X';
+
+    //a)
+    println(k + 12 * m);
+
+    //b)
+    println(m/j);
+
+    //c)
+    println(n%j);
+
+    //d)
+    println(f+ 10*5 +g);
+
+    //e)
+    //println(++i * n);
+}
