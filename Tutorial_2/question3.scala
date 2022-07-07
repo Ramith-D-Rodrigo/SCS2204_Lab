e48fb81eed75607bc3a981f106c257f695020cae
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2022-07-07 22:01:31 +0530

    edit some changes
    fixed the code so that it can execute

--- Tutorial_2/question3.scala
+++ Tutorial_2/question3.scala
@@ -5,9 +5,23 @@ object question3 extends App{
     var d: Int = 5;
 
     var k: Float = 4.3f;
-    println(--b * a + c * d--);
-    println(a++);
-    println(-2 * (g - k) + c);
-    println(c=c++);
-    println(c=++c*a++);
+    //println(--b * a + c * d--);
+    b = b - 1;
+    println(b*a + c*d);
+    d -= 1;
+    //println(a++);
+    println(a);
+    a += 1;
+    //println(-2 * (g - k) + c); g is not declared
+    
+    //println(c=c++);
+    println(c);
+    var temp : Int = c; //make sure c is not changed according to Java code
+    c += 1;
+    c = temp;
+    
+    //println(c=++c*a++);
+    c = c+1;
+    println(c*a);
+    a+=1;
 }
