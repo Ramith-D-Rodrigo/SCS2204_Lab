a0e6ab1868449420f268bff45b20f658f9cf10eb
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2022-07-12 09:51:29 +0530

    Create question5_a.scala

---
+++ Tutorial_3/question5_a.scala
@@ -0,0 +1,11 @@
+object question5 extends App{
+    var a: Int = 1;
+    var b: Int = 2;
+
+    if(a == 1)
+        if(b == 1)
+            println("***");
+        else
+            println("###");
+    println("===")
+}
