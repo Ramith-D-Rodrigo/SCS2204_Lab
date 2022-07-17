50e83e455ce1500d4088c46d00d0eda100c47176
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2022-07-17 22:17:52 +0530

    change the boolean expression in line 6,7 to match the question more accurately
    Since, first 50 copies get 3 shipping cost and MORE than 50 copies will get 0.75 per additional book.

--- Tutorial_3/question4.scala
+++ Tutorial_3/question4.scala
@@ -3,8 +3,8 @@ object question4 extends App{
         total_book_price(no_of_copies) - discount(no_of_copies) + shipping_cost(no_of_copies);
     }
     def shipping_cost(no_of_copies: Int): Double = no_of_copies match{
-        case x if x >= 50 => (3 * 50) + ((no_of_copies - 50) * 0.75);
-        case x if x < 50 => 3 * no_of_copies;
+        case x if x > 50 => (3 * 50) + ((no_of_copies - 50) * 0.75);
+        case x if x <= 50 => 3 * no_of_copies;
     }
     def total_book_price(no_of_copies: Int): Double = {
         no_of_copies * 24.95;
