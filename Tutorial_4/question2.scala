899937d3e2076f2906013a05fe434f50050c9467
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-07-26 22:35:13 +0530

    Update question2.scala

--- Tutorial_4/question2.scala
+++ Tutorial_4/question2.scala
@@ -1,9 +1,9 @@
 object question2 extends App{
     case class shopping_cart_item(name: String, price: Double, quantity: Int);
 
-    var item1 = shopping_cart_item("Vanilla ice cream", 3.99, 13);
-    var item2 = shopping_cart_item("Chocolate biscuits", 4, 6);
-    var item3 = shopping_cart_item("Cupcakes", 7.77, 7);
+    var item1 = shopping_cart_item("vanilla ice cream", 3.99, 13);
+    var item2 = shopping_cart_item("chocolate biscuits", 4, 6);
+    var item3 = shopping_cart_item("cupcakes", 7.77, 7);
 
     var shopping_cart = List(item1, item2, item3);
 
@@ -19,4 +19,22 @@ object question2 extends App{
     }
     print_items(shopping_cart);
 
+
+
+    def vanilla_and_others_print(cart: shopping_cart_item): Unit = cart match{
+        case (shopping_cart_item("vanilla ice cream",x,y)) => println(y + " vanilla ice cream at Rs"+x+" each");
+        case _ => println("Found another item");
+    }
+
+    def print_helpList(cart: List[shopping_cart_item]): Unit = {
+        if(cart.isEmpty){
+            return;
+        }
+        else{
+            vanilla_and_others_print(cart.head);
+            print_helpList(cart.tail);
+        }
+    }
+    println();
+    print_helpList(shopping_cart);
 }
