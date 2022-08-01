4468bbbb93338445b2fd75589d393b3cc1fc6f59
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-08-01 19:47:32 +0530

    Update question3.scala

--- Tutorial_4/question3.scala
+++ Tutorial_4/question3.scala
@@ -7,12 +7,20 @@ object question5 extends App{
         x.toLowerCase();
     }
 
-    def formatNames(name: String, some_func:(String) => String): String = {
-        some_func(name);
+    
+    def formatNames(name: String, some_func:(String) => String, change: List[Int]): String = {
+        if(change.head == -1){
+            some_func(name);
+        }
+        else{
+            var temp: Char = name.charAt(change.head);
+            name.replace(change.head) = some_func(temp.toString()).toCharArray()[0];
+            name;
+        }
     }
 
-    println(formatNames("Benny", toUpper));
-    println(formatNames("Ni", toUpper) + "roshan");
-    println(formatNames("Saman", toLower));
-    println(formatNames("K", toUpper) +"umar" + formatNames("a", toUpper));
+    println(formatNames("Benny", toUpper, List(-1)));
+    println(formatNames("Niroshan", toUpper, List(0,1)));
+    println(formatNames("Saman", toLower, List(-1)));
+    println(formatNames("Kumara", toUpper, List(0,5)));
 }
